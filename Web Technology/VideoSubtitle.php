f8b3aada5ed836a913f5121a2d43bd5001d0e93c
Sameer Ahmed <mallahgq225@gmail.com>
2018-11-03 05:41:47 -0700

    Add files via upload

---
+++ Web Technology/VideoSubtitle.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VideoSubtitles</title>
+</head>
+<body background="image.jpg">
+<center><video width="800" height="500" autoplay controls >
+	<track src="SubtitltesForVideo.vtt" kind="subtitles" >
+	<source src="Realityoflife" type="video/mp4"
+	srclang="en" label="English">
+	<source src="Realityoflife.ogg" type="video/ogg">
+</video></center>
+</body>
+</html>
---
+++ Web Technology/sameerwttask.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Sameer ahmed Task
+	</title>
+</head>
+
+
+<body>
+<header>
+	<h1 style="color: green">PAKISTAN</h1>
+   	<h4>Scope Of Web Technologies in Software Engineering</h4>
+    <p>  <mark>Software Engineering</mark> is very wide field and the web is the <del>sub</del> field of Software Engineering.<Sup>Web Technology</Sup> is very important software engineer for the Software Engineer Without web technology he can`t not introduce him self in the <mark>Market of Internet</mark> If you have any <sub>problem</sub> kindly contact for web develepment <a href="www.zaptox.com">Zaptox</a>
+    
+              </p>
+
+
+</header>
+
+
+<table style="text-decoration-color: white;" bgcolor="blue" border="4px solid red " cellpadding="5" cellspacing="5">
+		<tr >
+			<td >Row 1,Cell 1</td>
+			<td>Row 1,Cell 2</td>
+			<td>Row 1,Cell 3</td>
+		</tr>
+		<tr>
+			<td align="center">Row 2,Cell 1</td>
+			<td align="center">Row 2,Cell 2</td>
+			<td>Row 2,Cell 3</td>
+		</tr>
+		<tr>
+			<td align="center">Row 3,Cell 1</td>
+			<td align="center">Row 3,Cell 2</td>
+			<td align="right">  Row 3,Cell 3</td>
+		</tr>
+		
+<tr>
+			<td align="left" >Row 4,Cell 1</td>
+			<td>Row 4,Cell 2</td>
+			<td>Row 4,Cell 3</td>
+			
+		</tr>
+	
+
+
+		
+
+	</table>
+
+
+<h4> Making List in Html</h4>
+
+<ul type="cricle">
+<li> Making Order List </li>
+<ol type="a" start="1"> 
+<li> Item 1 </li>
+<li> Item 2 </li>
+<li> Item 3 </li>
+<li> Item 4 </li>
+
+</ol>	
+</ul>
+
+
+<img src="Coffee.jpg" alt="Image"	usemap="#workmap" width="500" height="300">  </img>
+
+<map name="workmap">
+	
+<area shape="circle" coords="120,150,60" href="https://www.google.com.pk/search?q=laptop-coffee-table-aerial-3289261" target="_blank">
+
+</map>
+<form method = "get" action="https://www.google.com.pk/search?">
+	
+	<p> Form Tags</p>
+     
+    <br>
+    Name :
+    <input type="text" >
+    <br>
+    Email :
+    <input type="text" >
+
+    <br>
+
+    Comments :
+    <br>
+    <textarea rows=" 10 " cols="15" placeholder="Type Comments Here"></textarea> 
+
+    <br>
+    Gender
+    <br>
+    
+		<input type="radio" value="Male" checked >Male
+		<br>
+		<br>
+		<input type="radio" value="Female">Female
+
+		<br>
+		<br>
+		
+		<select name="q"> 
+			<option>Select any country</option>
+			<option value="pakistan">Pakistan</option>
+			<option >India</option>
+			<option>China</option>
+			<option>USA</option>
+		</select>
+		<br>
+		<br>
+		<input type="submit" name="" value="submit">
+
+
+
+</form>
+	
+<footer></footer>
+</body>
+
+
+</html>
